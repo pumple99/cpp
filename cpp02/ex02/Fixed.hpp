33403978304b82be7bc837a175e7fb2fb56a95d1
pumple99 <ljhfree99@naver.com>
2023-07-26 16:13:37 +0900

    sync

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 16:55:05 by seunghoy          #+#    #+#             */
+/*   Updated: 2023/07/26 10:44:24 by seunghoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+private:
+	static const int	fractionalBits = 8;
+
+	int	rawBits;
+public:
+	Fixed();
+	Fixed(const int i);
+	Fixed(const float fl);
+	Fixed(const Fixed& f);
+	~Fixed();
+	
+	Fixed&	operator = (const Fixed& f);
+	Fixed	operator + (const Fixed& f);
+	Fixed	operator - (const Fixed& f);
+	Fixed	operator * (const Fixed& f);
+	Fixed	operator / (const Fixed& f);
+	bool	operator > (const Fixed& f);
+	bool	operator < (const Fixed& f);
+	bool	operator >= (const Fixed& f);
+	bool	operator <= (const Fixed& f);
+	bool	operator == (const Fixed& f);
+	bool	operator != (const Fixed& f);
+
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+};
+
+std::ostream& operator << (std::ostream& os, const Fixed& f);
+
+#endif
--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 16:55:08 by seunghoy          #+#    #+#             */
-/*   Updated: 2023/07/26 10:33:20 by seunghoy         ###   ########.fr       */
+/*   Updated: 2023/07/26 11:09:00 by seunghoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,10 @@
 
 int	main()
 {
-	Fixed a;
-	Fixed const b( -12 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-
-	a = Fixed( 1234.4321f );
-
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
+	Fixed a(0);
+	Fixed b( 838860 );
 	
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << b/a;
 
 	return 0;
 }
