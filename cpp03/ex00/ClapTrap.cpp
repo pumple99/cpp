158dee4a8ecbeb7a711c426b731ff8c392011503
pumple99 <ljhfree99@naver.com>
2023-08-08 23:48:30 +0900

    sync

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,85 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 22:52:45 by seunghoy          #+#    #+#             */
+/*   Updated: 2023/08/08 23:41:36 by seunghoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string& str)
+: _name(str), _hp(10), _energyPoint(10), _attackDamage(0)
+{
+	std::cout << "generate ClapTrap: " << _name << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "destory ClapTrap: " << _name << std::endl;
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (!this->_hp)
+	{
+		std::cout << "ClapTrap: " << this->_name << " is already dead.." << std::endl;
+		return ;
+	}
+	if (this->_energyPoint)
+	{
+		std::cout << "ClapTrap: " << this->_name << " attacks " << target;
+		std::cout << ", causing " << this->_attackDamage;
+		std::cout << " points of damage!" << std::endl;
+		--this->_energyPoint;
+	}
+	else
+		std::cout << "no energyPoint, cannot attack" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap: " << this->_name << " gets " << amount;
+	std::cout << " damage!" << std::endl;
+	if (!this->_hp)
+	{
+		std::cout << "ClapTrap: " << this->_name << " is already dead.." << std::endl;
+		return ;
+	}
+	if (this->_hp > amount)
+		this->_hp -= amount;
+	else
+	{
+		std::cout << "ClapTrap: " << this->_name << " died!" << std::endl;
+		this->_hp = 0;
+	}
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_energyPoint)
+	{
+		if (this->_hp)
+		{
+			std::cout << "ClapTrap: " << this->_name << " repairs ";
+			std::cout << amount << " hit points" << std::endl;
+			this->_hp += amount;
+		}
+		else
+		{
+			std::cout << "ClapTrap: " << this->_name << " has been revied with ";
+			std::cout << amount << " hit points" << std::endl;
+			this->_hp = amount;
+		}
+		--this->_energyPoint;
+	}
+	else
+		std::cout << "no energyPoint, cannot repair" << std::endl;
+}
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,34 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 22:46:08 by seunghoy          #+#    #+#             */
+/*   Updated: 2023/08/08 22:59:56 by seunghoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+private:
+	std::string		_name;
+	unsigned int	_hp;
+	unsigned int	_energyPoint;
+	unsigned int	_attackDamage;
+public:
+	ClapTrap(std::string& str);
+	~ClapTrap();
+
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+#endif
